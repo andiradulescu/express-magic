29c9c91e10ad8a67859dd7615d0a3f004f9ea5c1
Adam K Dean <adamkdean@googlemail.com>
2025-02-06 13:31:44 +0000

    chore: add initial v0.0.1 files

---
+++ index.js
@@ -0,0 +1,5 @@
+// Copyright (C) 2025 Adam K Dean <adamkdean@googlemail.com>
+// Use of this source code is governed by the GPL-3.0
+// license that can be found in the LICENSE file.
+
+// TODO: Implement
